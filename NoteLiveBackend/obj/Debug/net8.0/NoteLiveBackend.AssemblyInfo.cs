3a6806c5b3349e20dd42c4c6c6e66a33921f2913
erickpalomino1923 <erickpalomino0723@gmail.com>
2024-06-06 12:28:35 -0500

    sad

--- NoteLiveBackend/obj/Debug/net8.0/NoteLiveBackend.AssemblyInfo.cs
+++ NoteLiveBackend/obj/Debug/net8.0/NoteLiveBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteLiveBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5840bbb03783d1b1f29173ea6f4bc25b9242ab70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6af0ccae19dacf62dcb113f0408ef39e43299c68")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteLiveBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteLiveBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
