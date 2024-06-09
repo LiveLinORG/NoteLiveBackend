51ae3bc05821637aabe41e277910f882e26fd51b
JCarbajal22 <u202121881@upc.edu.pe>
2024-06-09 11:03:45 -0500

    Updated connection string for deploy

--- NoteLiveBackend/obj/Debug/net8.0/NoteLiveBackend.AssemblyInfo.cs
+++ NoteLiveBackend/obj/Debug/net8.0/NoteLiveBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteLiveBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c25ffd74d02735ff725210d8a25a6502d8f4aaf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0daf825651443d0a1190555d66a42d72411ecfd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteLiveBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteLiveBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
