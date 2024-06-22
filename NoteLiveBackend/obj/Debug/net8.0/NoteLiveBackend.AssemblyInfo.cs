e78935b4f00e69a7b3848b783c018b1ee194a5c1
APOgamer <adrianpalmaobi23@gmail.com>
2024-06-21 19:54:50 -0500

    Add: IAM

--- NoteLiveBackend/obj/Debug/net8.0/NoteLiveBackend.AssemblyInfo.cs
+++ NoteLiveBackend/obj/Debug/net8.0/NoteLiveBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteLiveBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0daf825651443d0a1190555d66a42d72411ecfd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ae3bc05821637aabe41e277910f882e26fd51b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteLiveBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteLiveBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
